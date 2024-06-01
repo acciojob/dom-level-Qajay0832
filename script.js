b79426f4cf95cbfd7260d55797aaadb91f86f8bd
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-06-01 19:23:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+//your JS code here. If requir
+let level=document.getElementById("level")
+let result=0;
+
+while (level.parentNode){
+        level=level.parentNode;
+        result+=1;
+}
+alert(`The level of the element is: ${result}`)
